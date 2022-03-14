f8635270cea13cff0c6f411c943405821b400c3c
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 10:36:43 +0100

    failing test

--- src/test/java/carfactorytest.java
+++ src/test/java/carfactorytest.java
@@ -7,10 +7,12 @@ public class carfactorytest {
 
     CarFactory carFactory;
     String colour = "blue";
+    String brand_1 = "volvo";
+    String brand_2 = "BMW";
 
     @BeforeEach
     void setUp(){
-        carFactory = new CarFactory();
+        carFactory = new CarFactory(brand_1);
     }
 
     @Test
@@ -21,5 +23,14 @@ public class carfactorytest {
         Assertions.assertEquals(colour,car.getColour());
         Assertions.assertNotNull(car.getCarBrand());
         Assertions.assertNotNull(car.getRegNumber());
+    }
+
+    @Test
+    void create_factory_with_brand_test(){
+        carFactory = new CarFactory(brand_2);
+
+        Car car = carFactory.create(colour);
+
+        Assertions.assertEquals(brand_2,car.getCarBrand());
     }
 }
