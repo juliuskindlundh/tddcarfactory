1462e4a328a4946fee62f38c0f3b889375360fed
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 10:31:17 +0100

    test pass

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,10 +1,12 @@
 public class Car {
     private String colour;
     private String brand;
+    private String regNumber;
 
     public Car(String colour,String factoryBrand) {
         this.colour = colour;
         this.brand = factoryBrand;
+        this.regNumber = "ABC123";
     }
 
     public String getColour() {
@@ -13,5 +15,9 @@ public class Car {
 
     public String getCarBrand() {
         return this.brand;
+    }
+
+    public String getRegNumber() {
+        return this.regNumber;
     }
 }
