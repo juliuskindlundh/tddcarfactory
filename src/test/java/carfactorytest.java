b80c39f8a1fe3af464f192d85048fa8315ef340d
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 09:48:45 +0100

    first failing test

---
+++ src/test/java/carfactorytest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class carfactorytest {
+
+    CarFactory carFactory;
+    String colour = "blue";
+
+    @BeforeEach
+    void setUp(){
+        carFactory = new CarFactory();
+    }
+
+    @Test
+    void test(){
+        Car car = carFactory.create();
+
+        Assertions.assertNotNull(car);
+        Assertions.assertEquals(colour,car.getColour());
+    }
+}
