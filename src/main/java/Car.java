f09cd84623ca788073c30681d128d5265bedc4b4
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 10:25:16 +0100

    test pass

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,11 +1,17 @@
 public class Car {
     private String colour;
+    private String brand;
 
-    public Car(String colour) {
+    public Car(String colour,String factoryBrand) {
         this.colour = colour;
+        this.brand = factoryBrand;
     }
 
     public String getColour() {
         return this.colour;
+    }
+
+    public String getCarBrand() {
+        return this.brand;
     }
 }
--- src/main/java/CarFactory.java
+++ src/main/java/CarFactory.java
@@ -1,5 +1,6 @@
 public class CarFactory {
+    private String factoryBrand = "volvo";
     public Car create(String colour) {
-        return new Car(colour);
+        return new Car(colour,factoryBrand);
     }
 }
